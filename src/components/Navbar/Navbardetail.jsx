95f01b33b72d0dffbb70e7a25777d76fd28b3670
ENG-MONA SALEH <0956675212m@gmail.com>
2024-03-20 12:16:58 +0300

    portfolio

---
+++ src/components/Navbar/Navbardetail.jsx
@@ -0,0 +1,26 @@
+export const ItemsDetail = [
+  {
+    title: 'Home',
+    url: '/mona-portfolio/',
+    className: 'navitems',
+    icon: 'fa-solid fa-house-chimney',
+  },
+  {
+    title: 'Resume',
+    url: '/mona-portfolio/resume',
+    className: 'navitems',
+    icon: 'fa-solid fa-file',
+  },
+  {
+    title: 'Work',
+    url: '/mona-portfolio/work',
+    className: 'navitems',
+    icon: 'fa-solid fa-briefcase',
+  },
+  {
+    title: 'Contact',
+    url: '/mona-portfolio/contact',
+    className: 'navitems',
+    icon: 'fa-solid fa-address-book',
+  },
+]
---
+++ src/components/Workproject/Workproject.jsx
@@ -0,0 +1,42 @@
+import '../Workproject/Workprojectstyle.css'
+
+import { Data } from './Workdata.js'
+const Workproject = () => {
+  return (
+    <>
+      <div className="work">
+        <div className="aboutline">
+          <h1>Portfolio</h1>
+          <div className="line"></div>
+        </div>
+        <div className="projects">
+          {Data.map((item) => {
+            return (
+              <a
+                key={item.name}
+                href={item.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="one">
+                  <img src={item.picture} alt="picture" />
+                  <p>{item.description}</p>
+                </div>
+              </a>
+            )
+          })}
+        </div>
+        <h1 className="h1">Certificates</h1>
+        <div className="certificate">
+          <img src="/mona-portfolio/images/arduino.jpg" alt="" />
+          <img src="/mona-portfolio/images/certificate.jpg" alt="" />
+          <img src="/mona-portfolio/images/hamak.jpg" alt="" />
+          <img src="/mona-portfolio/images/icdl.jpg" alt="" />
+          <img src="/mona-portfolio/images/tamayoz.jpg" alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Workproject
